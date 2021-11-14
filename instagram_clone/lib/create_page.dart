72d1328f6791af62b22c325e8947b426077d3eff
[strong1133] <strong1133@naver.com>
2021-11-14 15:18:19 +0900

    create page complete

--- instagram_clone/lib/create_page.dart
+++ instagram_clone/lib/create_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -6,10 +9,35 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
+  final textEditingController = TextEditingController();
+
+  File _image;
+  final picker = ImagePicker();
+
+  Future _getImage() async{
+    print('되나');
+    var image = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = File(image.path);
+    });
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImage,
+        child: Icon(Icons.add_a_photo),
+      ),
     );
   }
 
@@ -24,4 +52,19 @@ class _CreatePageState extends State<CreatePage> {
       ],
     );
   }
+
+  Widget _buildBody(){
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _image == null ? Text('No Image') : Image.file(_image),
+          TextField(
+            controller: textEditingController,
+            decoration: InputDecoration(hintText: '내용을 입력하세요.'),
+          )
+        ],
+      ),
+    );
+  }
+
 }

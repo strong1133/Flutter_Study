15acaff7316e43ce5a8743595d3c91e63ea92dc4
[strong1133] <strong1133@naver.com>
2021-11-16 23:36:44 +0900

    함수 생성자

--- Dart/class01.dart
+++ Dart/class01.dart
@@ -1,15 +1,24 @@
 // 클래스 - 2021.11.16
 
 class Person {
-  String _name;
-  int _age;
+  String? _name;
+  int? _age;
 
-  String showInfo(Person person) {
-    return "${person._name} 님의 나이는 ${person._age} 살 입니다.";
+  // 기본 생성자
+  Person() {}
+
+  // 이름과 나이를 매개변수로 받는 생성자
+  Person.gen(String name, int age) {
+    this._name = name;
+    this._age = age;
   }
 
-  Person(String _name, int _age){
-    this._name = _name; 
-    this._age = _age;
-  };
+  String showInfo(Person person) {
+    return "${person._name}님의 나이는 ${person._age}살 입니다.";
+  }
+}
+
+void main() {
+  Person person = new Person.gen("정석진", 12);
+  print(person.showInfo(person));
 }

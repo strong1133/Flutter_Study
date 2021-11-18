cf125043c93ffdd5a13b57c446e4cd2d0fd4b5e5
[strong1133] <strong1133@naver.com>
2021-11-18 23:53:59 +0900

    Account Page

--- cpt02/instagram_clone/lib/account_page.dart
+++ cpt02/instagram_clone/lib/account_page.dart
@@ -8,8 +8,94 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Account'),
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.exit_to_app),
+          color: Colors.black,
+        )
+      ],
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  SizedBox(
+                    width: 80.0,
+                    height: 80.0,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fca143133-349a-4075-a68b-60ee33b318c9%2FSJ_Main.png?table=block&id=fa0eb946-388d-40b5-9295-9e29d1eca65f&spaceId=89c8c53a-d004-445f-8b96-e4a9cd8ffa7c&width=250&userId=582806da-b4e3-4c8c-8573-a1b75f56bbb5&cache=v2'),
+                    ),
+                  ),
+                  Container(
+                    width: 82.0,
+                    height: 82.0,
+                    alignment: Alignment.bottomRight,
+                    child: SizedBox(
+                      width: 29.0,
+                      height: 29.0,
+                      child: FloatingActionButton(
+                        onPressed: null,
+                        backgroundColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    height: 80.0,
+                    alignment: Alignment.bottomRight,
+                    child: SizedBox(
+                      width: 25.0,
+                      height: 25.0,
+                      child: FloatingActionButton(
+                        onPressed: null,
+                        backgroundColor: Colors.blue,
+                        child: Icon(Icons.add),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(6.0)),
+              Text('이름', style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Text(
+            '0\n게시물',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '0\n팔로워',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '0\n팔로잉',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          Padding(padding: EdgeInsets.all(2.0)),
+        ],
+      ),
     );
   }
 }

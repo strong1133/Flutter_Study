f6dcf8b6a0aa9dc628ed89bd47c8bada37a17f95
CJ-12074456\plmons44 <strong1133@naver.com>
2021-11-18 16:32:12 +0900

    Conditional Basic

---
+++ Dart/conditional02.dart
@@ -0,0 +1,20 @@
+// 분기 - 2021.11.18
+
+void main(){
+  String score = "A";
+
+  switch (score){
+    case 'A':
+      print("학점 4.0");
+      break;
+    case 'B':
+      print("학점 3.0");
+      break;
+    case 'C':
+      print("학점 2.0");
+      break;
+    case 'F':
+      print("낙제");
+      break;
+  }
+}

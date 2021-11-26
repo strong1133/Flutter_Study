cc243832f2481edcd29118068cfeb139d07b4c60
[strong1133] <strong1133@naver.com>
2021-11-27 00:19:55 +0900

    순한맛 14 App bar

--- codingChef/class02/flutter_class02/lib/main.dart
+++ codingChef/class02/flutter_class02/lib/main.dart
@@ -5,6 +5,46 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AppBar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AppBar Icon Menu"),
+        centerTitle: true,
+        elevation: 0.0,
+
+        //AppBar 왼쪽에 버튼이 생김
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print("Menu Button is Clicked");
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print("shopping_cart Button is Clicked");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("search Button is Clicked");
+            },
+          ),
+        ],
+      ),
+    );
   }
 }

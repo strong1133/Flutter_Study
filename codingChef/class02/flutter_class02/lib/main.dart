9ca0f7073cb0457c6832d40034c71afb1b8222c5
[strong1133] <strong1133@naver.com>
2021-11-27 02:05:18 +0900

    순한맛 18 | SnackBar

--- codingChef/class02/flutter_class02/lib/main.dart
+++ codingChef/class02/flutter_class02/lib/main.dart
@@ -19,104 +19,133 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("AppBar Icon Menu"),
-        centerTitle: true,
-        elevation: 0.0,
+      appBar: _buildAppBar(),
+      // drawer 메뉴를 추가하면 자동으로 왼쪽에 메뉴 버튼이 생김.
+      drawer: _buildDrawer(flag),
+      body: _buildBody(context),
+    );
+  }
 
-        //AppBar 왼쪽에 버튼이 생김
-        // leading: IconButton(
-        //   icon: Icon(Icons.menu),
-        //   onPressed: () {
-        //     print("Menu Button is Clicked");
-        //   },
-        // ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print("shopping_cart Button is Clicked");
+  // AppBar는 AppBar type으로
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text("AppBar Icon Menu"),
+      centerTitle: true,
+      elevation: 0.0,
+
+      //AppBar 왼쪽에 버튼이 생김
+      // leading: IconButton(
+      //   icon: Icon(Icons.menu),
+      //   onPressed: () {
+      //     print("Menu Button is Clicked");
+      //   },
+      // ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {
+            print("shopping_cart Button is Clicked");
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            print("search Button is Clicked");
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDrawer(flag) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('assets/sj_main.png'),
+            ),
+            otherAccountsPictures: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/truper.png'),
+              ),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/baider.png'),
+              ),
+            ],
+            accountName: Text("정석진"),
+            accountEmail: Text("strong3879@gmail.com"),
+            onDetailsPressed: () {
+              if (flag) {
+                print("펼쳐졌다!");
+                flag = false;
+                return;
+              }
+              print("접혀졌다!");
+              flag = true;
             },
+            decoration: BoxDecoration(
+              color: Colors.red[400],
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15.0),
+                  bottomRight: Radius.circular(15.0)),
+            ),
           ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print("search Button is Clicked");
+          ListTile(
+            leading: Icon(
+              Icons.home,
+              color: Colors.grey[850],
+            ),
+            title: Text('Home'),
+            onTap: () {
+              print('Home Button 눌림!');
             },
+            trailing: Icon(Icons.add),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Colors.grey[850],
+            ),
+            title: Text('Setting'),
+            onTap: () {
+              print('Setting 눌림!');
+            },
+            trailing: Icon(Icons.add),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.question_answer,
+              color: Colors.grey[850],
+            ),
+            title: Text('Q&A'),
+            onTap: () {
+              print('Q&A 눌림!');
+            },
+            trailing: Icon(Icons.add),
           ),
         ],
       ),
-      // drawer 메뉴를 추가하면 자동으로 왼쪽에 메뉴 버튼이 생김.
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/sj_main.png'),
-              ),
-              otherAccountsPictures: <Widget>[
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/truper.png'),
-                ),
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/baider.png'),
-                ),
-              ],
-              accountName: Text("정석진"),
-              accountEmail: Text("strong3879@gmail.com"),
-              onDetailsPressed: () {
-                if (flag) {
-                  print("펼쳐졌다!");
-                  flag = false;
-                  return;
-                }
-                print("접혀졌다!");
-                flag = true;
-              },
-              decoration: BoxDecoration(
-                color: Colors.red[400],
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(15.0),
-                    bottomRight: Radius.circular(15.0)),
-              ),
+    );
+  }
+
+  Widget _buildBody(context) {
+    return Builder(
+      builder: (BuildContext ctx) {
+        return Center(
+          child: FlatButton(
+            child: Text(
+              'Show Me',
+              style: TextStyle(color: Colors.white),
             ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('Home'),
-              onTap: () {
-                print('Home Button 눌림!');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('Setting'),
-              onTap: () {
-                print('Setting 눌림!');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('Q&A'),
-              onTap: () {
-                print('Q&A 눌림!');
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],
-        ),
-      ),
+            color: Colors.red,
+            onPressed: () {
+              Scaffold.of(ctx).showSnackBar(SnackBar(content: Text('Hello')));
+            },
+          ),
+        );
+      },
     );
   }
 }

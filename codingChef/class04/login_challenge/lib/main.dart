8a6bf947867de08dd9d051ec4d1dc50e4e8cda91
[strong1133] <strong1133@naver.com>
2021-11-28 15:52:14 +0900

    조금매운맛7 | start

---
+++ codingChef/class04/login_challenge/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Sign in',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}

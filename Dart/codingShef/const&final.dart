7273ba88ab287dadda77829462ff57a8a42ae9bd
[strong1133] <strong1133@naver.com>
2021-11-28 13:27:18 +0900

    조금매운맛6 | final,const

---
+++ Dart/codingShef/const&final.dart
@@ -0,0 +1,12 @@
+void main() {
+  final int myFinal = 30;
+  const int myConst = 70;
+
+  // myFinal = 20;
+  // myConst = 50;
+
+  final timeFinal = DateTime.now();
+  // const timeConst = DateTime.now();
+  print(timeFinal);
+  // print(timeConst)
+}

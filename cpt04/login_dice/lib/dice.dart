67b96f739a0eece5b7b6450afa03f2fdfe4b21be
[strong1133] <strong1133@naver.com>
2021-11-29 23:41:41 +0900

    dice 2차 완료

--- cpt04/login_dice/lib/dice.dart
+++ cpt04/login_dice/lib/dice.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Dice extends StatefulWidget {
   @override
@@ -6,6 +9,9 @@ class Dice extends StatefulWidget {
 }
 
 class _DiceState extends State<Dice> {
+  int leftDice = 1;
+  int rightDice = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,20 +29,48 @@ class _DiceState extends State<Dice> {
               padding: EdgeInsets.all(30.0),
               child: Row(
                 children: <Widget>[
-                  Expanded(child: Image.asset('assets/dice1.png')),
+                  Expanded(child: Image.asset('assets/dice$leftDice.png')),
                   SizedBox(
                     width: 20.0,
                   ),
-                  Expanded(child: Image.asset('assets/dice2.png')),
+                  Expanded(child: Image.asset('assets/dice$rightDice.png')),
                 ],
               ),
             ),
             SizedBox(
               height: 30.0,
             ),
+            ButtonTheme(
+              minWidth: 100.0,
+              height: 60.0,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    leftDice = Random().nextInt(6) + 1;
+                    rightDice = Random().nextInt(6) + 1;
+                  });
+                  showToast("Left Dice: $leftDice, Right Dice: $rightDice");
+                },
+                color: Colors.amber,
+                child: Icon(
+                  Icons.play_arrow,
+                  size: 50.0,
+                  color: Colors.white,
+                ),
+              ),
+            )
           ],
         ),
       ),
     );
   }
+}
+
+void showToast(String msg) {
+  Fluttertoast.showToast(
+      msg: msg,
+      backgroundColor: Colors.white,
+      textColor: Colors.black,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM);
 }

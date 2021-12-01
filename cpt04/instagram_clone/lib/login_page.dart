3e2125b1b90a5baebfbae85e072290d07ee33753
[strong1133] <strong1133@naver.com>
2021-12-02 00:24:36 +0900

    login Page start

---
+++ cpt04/instagram_clone/lib/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Instagram Clone',
+            style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- cpt04/instagram_clone/lib/root_page.dart
+++ cpt04/instagram_clone/lib/root_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/login_page.dart';
 import 'package:instagram_clone/tab_page.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }

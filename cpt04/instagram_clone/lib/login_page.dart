3697fe1e97772404a619cc204992b12ba8c1ae75
[strong1133] <strong1133@naver.com>
2021-12-02 00:50:31 +0900

    firebase auth print

--- cpt04/instagram_clone/lib/login_page.dart
+++ cpt04/instagram_clone/lib/login_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,18 +10,40 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Instagram Clone',
-            style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Instagram Clone',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+            Padding(padding: EdgeInsets.all(50.0)),
+            SignInButton(Buttons.Google, onPressed: () {
+              _handleSignIn().then((user) => print(user));
+            })
+          ],
+        ),
       ),
     );
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    print(googleUser.email);
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = (await _auth.signInWithCredential(
+            GoogleAuthProvider.getCredential(
+                idToken: googleAuth.idToken,
+                accessToken: googleAuth.accessToken)))
+        .user;
+
+    return user;
   }
 }

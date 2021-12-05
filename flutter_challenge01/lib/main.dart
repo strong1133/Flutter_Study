fe1274aa76db842375d0390bef83c976ee906caa
strong1133 <strong1133@naver.com>
2021-12-05 22:46:28 +0900

    challenge01 Random Word complete

--- flutter_challenge01/lib/main.dart
+++ flutter_challenge01/lib/main.dart
@@ -4,19 +4,57 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final wordPair = WordPair.random();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Flutter Challenge01",
       home: Scaffold(
         appBar: AppBar(
           title: const Text('플러터 첼린지 1'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        body: Center(child: RandomWord()),
+      ),
+    );
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  _RandomWordState createState() => _RandomWordState();
+}
+
+class _RandomWordState extends State<RandomWord> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    print(wordPair);
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (_context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asCamelCase,
+        style: _biggerFont,
       ),
     );
   }

6f657369530a1784e4a1497239c89b6fd82c4695
strong1133 <strong1133@naver.com>
2021-12-06 00:57:41 +0900

    snackbar 찍어보기

--- codingChef/class04/context_and_snackbar/lib/main.dart
+++ codingChef/class04/context_and_snackbar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +19,47 @@ class _MyAppState extends State<MyApp> {
           title: Text('Context & SnackBar'),
         ),
         floatingActionButton: ActionButtonExample(),
+        body: _buildBody(),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Builder(builder: (BuildContext context1) {
+              return Container(
+                color: Colors.amber,
+                child: Column(children: <Widget>[
+                  IconButton(
+                      onPressed: () {
+                        showSnackBar(context1, "1");
+                      },
+                      icon: Icon(Icons.ac_unit)),
+                  Builder(builder: (BuildContext context2) {
+                    return Container(
+                      color: Colors.amber[100],
+                      child: Column(children: <Widget>[
+                        IconButton(
+                            onPressed: () {
+                              showSnackBar(context2, "2");
+                            },
+                            icon: Icon(Icons.ac_unit)),
+                      ]),
+                      width: 200.0,
+                      height: 200.0,
+                    );
+                  })
+                ]),
+                width: 300.0,
+                height: 300.0,
+              );
+            })
+          ],
+        ),
       ),
     );
   }
@@ -28,14 +70,20 @@ class ActionButtonExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: () {
-        Scaffold.of(context).showSnackBar(new SnackBar(
-          backgroundColor: Colors.blue,
-          content: Builder(builder: (context) {
-            return Text('Snack Bar');
-          }),
-        ));
+        showSnackBar(context, "btn");
       },
       child: Icon(Icons.ac_unit),
     );
   }
+}
+
+void showSnackBar(BuildContext context, String loc) {
+  Scaffold.of(context).showSnackBar(new SnackBar(
+    backgroundColor: Colors.blue,
+    content: Builder(builder: (context) {
+      print(context.toString());
+      return Text('Snack Bar on $loc');
+    }),
+    duration: Duration(seconds: 1),
+  ));
 }

81b5c7649117c561d25fd8864a1517c1b6728ca5
strong1133 <strong1133@naver.com>
2021-12-08 00:45:31 +0900

    날씨앱 시작

--- codingChef/class05/flutter_weather/lib/main.dart
+++ codingChef/class05/flutter_weather/lib/main.dart
@@ -1,2 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather/screens/loading.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Weather App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Loading());
+  }
+}

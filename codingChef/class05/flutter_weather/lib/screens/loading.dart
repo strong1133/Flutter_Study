f6b2d73e0017118d9f6fba257166b14d1740d147
strong1133 <strong1133@naver.com>
2021-12-11 22:17:27 +0900

    날씨 받아오기

--- codingChef/class05/flutter_weather/lib/screens/loading.dart
+++ codingChef/class05/flutter_weather/lib/screens/loading.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   @override
@@ -7,12 +11,45 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getLocation();
+    _fetchData();
+  }
+
+  void _getLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print('position: $position');
+    } catch (e) {
+      print('에러!');
+    }
+  }
+
+  void _fetchData() async {
+    final queryParameters = {
+      'q': 'London',
+      'appid': 'b1b15e88fa797225412429c1c50c122a1',
+    };
+    const baseUrl = 'samples.openweathermap.org';
+    final uri = Uri.https(baseUrl, '/data/2.5/weather', queryParameters);
+    final response = await http.get(uri);
+    String jsonData = response.body;
+    var weather = jsonDecode(jsonData)['weather'][0]['description'];
+    var wind = jsonDecode(jsonData)['wind']['speed'];
+    var id = jsonDecode(jsonData)['id'];
+    print('$weather, $wind, $id');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {},
-          child: Text('Get My Location'),
+          onPressed: null,
+          child: const Text('Get My Location'),
         ),
       ),
     );

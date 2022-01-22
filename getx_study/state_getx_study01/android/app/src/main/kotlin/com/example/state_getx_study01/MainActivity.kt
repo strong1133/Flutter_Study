30550fc105069fe076875d776a6000f8e5048fb1
[strong1133] <strong1133@naver.com>
2022-01-22 16:39:05 +0900

    GetX_Study Project Start

---
+++ getx_study/state_getx_study01/android/app/src/main/kotlin/com/example/state_getx_study01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.state_getx_study01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ getx_study/state_getx_study01/lib/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetX Study'),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Text('Hello World'),
+          ],
+        ),
+      ),
+    );
+  }
+}

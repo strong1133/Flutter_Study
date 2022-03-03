6f568e6bccb4322fd465f5b9fdc4e8473d8fba54
strong1133 <strong1133@naver.com>
2022-03-03 09:01:24 +0900

    Flutter 리팩토링

--- getx_study/state_getx_study01/lib/widgets/calendar/month_view_widget.dart
+++ getx_study/state_getx_study01/lib/widgets/calendar/month_view_widget.dart
@@ -8,11 +8,19 @@ class MonthViewWidget extends StatelessWidget {
   final double? width;
   dynamic? ratio;
 
+  EventController eventController;
+  final event = CalendarEventData(
+    date: DateTime(2022, 2, 10),
+    event: "Event 1",
+    title: 'asd',
+  );
+
   MonthViewWidget({Key? key, this.state, this.width, this.ratio})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    eventController.add(event);
     return MonthView<Event>(
       onCellTap: (events, date) => {print("events  $events =  |  date  $date")},
       key: state,

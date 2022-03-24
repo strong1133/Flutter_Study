b8d9f7b563145d6ac0685059a209f347169f9f2d
strong1133 <strong1133@naver.com>
2022-03-24 13:43:13 +0900

    radio btn

---
+++ getx_study/flutter_radio_btn/android/app/src/main/kotlin/com/example/flutter_radio_btn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_radio_btn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ getx_study/flutter_radio_btn/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+enum Dispacth { t, f }
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  Dispacth _dispacth = Dispacth.t;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: const Text('Lafayette'),
+          leading: Radio(
+            value: Dispacth.t,
+            groupValue: _dispacth,
+            onChanged: (Dispacth? value) {
+              print(value);
+              setState(() {
+                _dispacth = value!;
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: const Text('Lafayette'),
+          leading: Radio(
+            value: Dispacth.f,
+            groupValue: _dispacth,
+            onChanged: (Dispacth? value) {
+              print(value);
+              setState(() {
+                _dispacth = value!;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}

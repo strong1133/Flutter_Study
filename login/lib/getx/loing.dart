81b5c2ae1947b1b3bc74fca0f84a696c36075d75
strong1133 <strong1133@naver.com>
2022-09-05 10:00:02 +0900

    textfiled 구성중

--- login/lib/getx/loing.dart
+++ login/lib/getx/loing.dart
@@ -8,25 +8,46 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.maxFinite,
-      height: double.maxFinite,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.fill,
-          image: AssetImage('images/login_background.png')
-        )
-      ),
-      child: Center(
-        child:  Container(
-          width: 500,
-          height: 400,
-          decoration: BoxDecoration(
-            border: Border.all(width: 1.0, color: Colors.white),
-            borderRadius: BorderRadius.circular(15),
+        width: double.maxFinite,
+        height: double.maxFinite,
+        decoration: BoxDecoration(image: DecorationImage(fit: BoxFit.fill, image: AssetImage('images/login_background.png'))),
+        child: Center(
+          child: Container(
+            width: 500,
+            height: 400,
+            // decoration: BoxDecoration(
+            //   border: Border.all(width: 1.0, color: Colors.white),
+            //   borderRadius: BorderRadius.circular(15),
+            // ),
+            child: Column(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 1, color: Colors.black38),
+                              borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0)),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 1, color: Colors.black38),
+                              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(8.0), bottomRight: Radius.circular(8.0)),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-          child: Text('asd'),
-        ),
-      )
-    );
+        ));
   }
 }

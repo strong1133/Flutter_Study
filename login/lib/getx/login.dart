7f083259c58cea0de48dbf94f09b72ef4e15c340
strong1133 <strong1133@naver.com>
2022-09-08 09:33:42 +0900

    버튼 구성

--- login/lib/getx/login.dart
+++ login/lib/getx/login.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:login/widgets/inputter.dart';
 
 class Login extends StatelessWidget {
@@ -11,10 +9,10 @@ class Login extends StatelessWidget {
     return Container(
         width: double.maxFinite,
         height: double.maxFinite,
-        decoration: BoxDecoration(image: DecorationImage(fit: BoxFit.fill, image: AssetImage('images/login_background.png'))),
+        decoration: const BoxDecoration(image: DecorationImage(fit: BoxFit.fill, image: AssetImage('images/login_background.png'))),
         child: Center(
-          child: Container(
-            width: 500,
+          child: SizedBox(
+            width: 400,
             height: 400,
             // decoration: BoxDecoration(
             //   border: Border.all(width: 1.0, color: Colors.white),
@@ -25,15 +23,70 @@ class Login extends StatelessWidget {
                 Column(
                   children: [
                     Inputter(
+                      placeHolder: 'ID',
                       top: true,
-                      onChanged: (e){
-                      print("TOP ${e}");
-                    },),
+                      onChanged: (e) {
+                        print("TOP ${e}");
+                      },
+                    ),
                     Inputter(
+                      placeHolder: 'PW',
+                      isSecret: true,
                       bottom: true,
-                      onChanged: (e){
-                      print("Bottom ${e}");
-                    },)
+                      onChanged: (e) {
+                        print("Bottom ${e}");
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: 35,
+                      width: double.maxFinite,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.resolveWith((states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return Colors.green[400];
+                              } else {
+                                return Colors.green;
+                              }
+                            }),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            '로그인',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    SizedBox(
+                      height: 35,
+                      width: double.maxFinite,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.resolveWith((states) {
+                              if (states.contains(MaterialState.pressed)) {
+                                return Colors.yellow[200];
+                              } else {
+                                return Colors.yellow;
+                              }
+                            }),
+                          ),
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset('images/kakao_icon.png'),
+                              const Text(
+                                ' 카카오톡으로 로그인',
+                                style: TextStyle(color: Colors.black, fontWeight: FontWeight.w800),
+                              )
+                            ],
+                          )),
+                    )
                   ],
                 )
               ],
--- login/lib/widgets/inputter.dart
+++ login/lib/widgets/inputter.dart
@@ -4,29 +4,28 @@ class Inputter extends StatelessWidget {
   Function onChanged;
   bool? top;
   bool? bottom;
-  Inputter({required this.onChanged, this.top = false, this.bottom=false, Key? key}) : super(key: key);
+  bool? isSecret;
+  String? placeHolder;
+  Inputter({required this.onChanged, this.top = false, this.bottom = false, this.isSecret = false, this.placeHolder = '', Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    BorderRadius borderRadius =  const BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0));
-    if(top == true){
+    BorderRadius borderRadius = const BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0));
+    if (top == true) {
       borderRadius = const BorderRadius.only(topLeft: Radius.circular(8.0), topRight: Radius.circular(8.0));
-    }else if(bottom == true){
+    } else if (bottom == true) {
       borderRadius = const BorderRadius.only(bottomLeft: Radius.circular(8.0), bottomRight: Radius.circular(8.0));
     }
     return TextField(
-      onChanged: (e){
+      onChanged: (e) {
         onChanged(e);
       },
+      obscureText: isSecret!,
+      cursorColor: Colors.black,
       decoration: InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderSide: const BorderSide(width: 1, color: Colors.black38),
-            borderRadius: borderRadius
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: const BorderSide(width: 1, color: Colors.black38),
-            borderRadius: borderRadius
-          ),
+          hintText: '$placeHolder',
+          focusedBorder: OutlineInputBorder(borderSide: const BorderSide(width: 1, color: Colors.black38), borderRadius: borderRadius),
+          enabledBorder: OutlineInputBorder(borderSide: const BorderSide(width: 1, color: Colors.black38), borderRadius: borderRadius),
           filled: true,
           fillColor: Colors.white),
     );

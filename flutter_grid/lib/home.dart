789c048ebe869f10692a159f6ea7b450ea0262c8
strong1133 <strong1133@naver.com>
2022-09-20 23:02:17 +0900

    표만들기 ui 하드코딩

--- flutter_grid/lib/home.dart
+++ flutter_grid/lib/home.dart
@@ -8,33 +8,109 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-         SizedBox(
-            width: 50,
-            height: 500,
-            child: GridView.count(
-              crossAxisCount: 1,
-              children: List.generate(3, (index) {
-                return Container(
-                  decoration: BoxDecoration(border: Border.all(), color: Colors.red),
-                  child: Text('idx $index'),
-                );
-              }),
+          Container(
+            width: 330,
+            child: Table(
+               columnWidths: const {
+              0: FractionColumnWidth(.1),
+            },
+              children: [
+                TableRow(children: [
+                  Container(
+                    height: 30,
+                    color: Colors.red,
+                    child: const Text("6"),
+                  ),
+                  Container(
+                    height: 30,
+                    color: Colors.grey,
+                    child: const Text("6"),
+                  ),
+                ]),
+              ],
             ),
           ),
-          SizedBox(
-            width: 250,
-            height: 500,
-            child: GridView.count(
-              crossAxisCount: 3,
-              children: List.generate(12, (index) {
-                return Container(
-                  decoration: BoxDecoration(border: Border.all(), color: Colors.red),
-                  child: Text('idx $index'),
-                );
-              }),
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 30.0,
+                child: Table(
+                  children: [
+                    TableRow(children: [
+                      Container(
+                        color: Colors.cyan,
+                        width: 50.0,
+                        height: 100.0,
+                        child: const Text("6"),
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+              Container(
+                width: 300,
+                child: Table(
+                  children: [
+                    TableRow(children: [
+                      Container(
+                        color: Colors.green,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("1"),
+                      ),
+                      Container(
+                        color: Colors.red,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("2"),
+                      ),
+                      Container(
+                        color: Colors.amberAccent,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("3"),
+                      ),
+                      Container(
+                        color: Colors.black87,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("4"),
+                      ),
+                    ]),
+                    TableRow(children: [
+                      Container(
+                        color: Colors.amber,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("5"),
+                      ),
+                      Container(
+                        color: Colors.blue,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("6"),
+                      ),
+                      Container(
+                        color: Colors.purple,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("7"),
+                      ),
+                      Container(
+                        color: Colors.blueGrey,
+                        width: 50.0,
+                        height: 50.0,
+                        child: const Text("8"),
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),

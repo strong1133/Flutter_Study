b4abb36e9e05d52a45f8a9c5b7d2d42ccd28574b
strong3879@gmail.com <strong3879@gmail.com>
2022-10-06 23:39:45 +0900

    Default Layout AppBar 기본틀 생성

--- inflearn/actual/lib/common/layout/default_layout.dart
+++ inflearn/actual/lib/common/layout/default_layout.dart
@@ -5,13 +5,43 @@ import 'package:flutter/src/widgets/framework.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
-  const DefaultLayout({required this.child, this.backgroundColor = Colors.white, Key? key}) : super(key: key);
+  final String? title;
+  final Widget? bottomNavigationBar;
+
+  const DefaultLayout(
+      {required this.child,
+      this.bottomNavigationBar,
+      this.title,
+      this.backgroundColor = Colors.white,
+      Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: child,
+      appBar: renderAppbar(),
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppbar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        automaticallyImplyLeading:false,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(title!,
+        style: TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.w500
+        ),),
+        foregroundColor: Colors.black,
+      
+      );
+    }
   }
 }
--- inflearn/actual/lib/common/view/root_tab.dart
+++ inflearn/actual/lib/common/view/root_tab.dart
@@ -1,3 +1,4 @@
+import 'package:actual/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -7,8 +8,9 @@ class RootTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Root Tab')),
+    return DefaultLayout(
+      title: '코팩 딜리버리',
+      child: Center(child: Text('Root Tab')),
     );
   }
 }

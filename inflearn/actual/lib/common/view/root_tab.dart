d7b48b147bd4de1b8e53a57005d9d30be22c7716
strong3879@gmail.com <strong3879@gmail.com>
2022-10-06 23:48:39 +0900

    BottomNavigationBar 생성

--- inflearn/actual/lib/common/view/root_tab.dart
+++ inflearn/actual/lib/common/view/root_tab.dart
@@ -1,16 +1,39 @@
+import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int idx =0;
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '코팩 딜리버리',
-      child: Center(child: Text('Root Tab')),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedItemColor: PRIMARY_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed,
+        onTap: (int idx) {
+          setState(() {
+            this.idx = idx;
+          });
+        },
+        currentIndex: idx,
+        items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+        BottomNavigationBarItem(icon: Icon(Icons.fastfood_outlined), label: '음식'),
+        BottomNavigationBarItem(icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+        BottomNavigationBarItem(icon: Icon(Icons.percent_outlined), label: '프로필'),
+      ]),
+      child: const Center(child: Text('Root Tab')),
     );
   }
 }

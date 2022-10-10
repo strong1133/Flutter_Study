5d22a3299a3d081d8cb8ca51822d1a50f8aa5b75
strong3879@gmail.com <strong3879@gmail.com>
2022-10-10 16:36:03 +0900

    restaurant cart sample

--- inflearn/actual/lib/common/view/root_tab.dart
+++ inflearn/actual/lib/common/view/root_tab.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/restaurant/view/restaurant_screen.dart';
 import 'package:flutter/material.dart';
 
 class RootTab extends StatefulWidget {
@@ -11,7 +12,7 @@ class RootTab extends StatefulWidget {
 
 class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
   late TabController tabController;
-    int idx = 0;
+  int idx = 0;
 
   @override
   void initState() {
@@ -22,7 +23,7 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
     tabController.addListener(tabListhener);
   }
 
-  void tabListhener(){
+  void tabListhener() {
     setState(() {
       idx = tabController.index;
     });
@@ -34,7 +35,6 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
     // TODO: implement dispose
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -60,13 +60,14 @@ class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
               BottomNavigationBarItem(
                   icon: Icon(Icons.percent_outlined), label: '프로필'),
             ]),
-        child: TabBarView(controller: tabController, children: [
-          Container(
-            child: Center(child: Text('홈')),
-          ),
-          Container(child: Center(child: Text('음식'))),
-          Container(child: Center(child: Text('주문'))),
-          Container(child: Center(child: Text('프로필'))),
-        ]));
+        child: TabBarView(
+            controller: tabController,
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              RestaurantScreen(),
+              Container(child: Center(child: Text('음식'))),
+              Container(child: Center(child: Text('주문'))),
+              Container(child: Center(child: Text('프로필'))),
+            ]));
   }
 }

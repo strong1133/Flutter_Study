ad8b872fde4f4ec1077bf6ea7f6ca481ad047162
strong3879@gmail.com <strong3879@gmail.com>
2022-11-03 23:55:59 +0900

    image picker 여러개 샘플링

--- Flutter/Widget/img_picker_study/lib/home.dart
+++ Flutter/Widget/img_picker_study/lib/home.dart
@@ -1,10 +1,9 @@
-
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:images_picker/images_picker.dart';
 
 class Home extends StatefulWidget {
   Home();
@@ -15,45 +14,94 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   PickedFile _image;
+  List<Media> _images = [];
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: [
-
-          SizedBox(height: 30,),
-
+          SizedBox(
+            height: 30,
+          ),
           Container(
             width: 100,
             height: 100,
             child: Center(
-              child: _image == null? Text('No Image'):
-              Image.file(File(_image.path)),
+              child: _image == null ? Text('No Image') : Image.file(File(_image.path)),
             ),
           ),
-          SizedBox(height: 30,),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                ..._images.map(
+                  (e) {
+                    return Container(
+                      width: 100,
+                      height: 100,
+                      child: Center(
+                        child: Image.file(File(e.path)),
+                      ),
+                    );
+                  },
+                ).toList()
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          // ElevatedButton(
+          //     onPressed: () {
+          //       getImageFromCamera();
+          //       log('CAMERA');
+          //     },
+          //     child: Text('Camera')),
+          // ElevatedButton(
+          //     onPressed: () {
+          //       getImageFromGallery();
+          //     },
+          //     child: Text('Gallery')),
           ElevatedButton(
               onPressed: () {
-                getImageFromCamera();
+                getImageFromGallery();
               },
-              child: Text('Camera')),
-          ElevatedButton(onPressed: () {}, child: Text('Gallery'))
+              child: Text('Gallery Multi'))
         ],
       ),
     );
   }
 
-  Future getImageFromCamera() async {
-    var image = await ImagePicker.platform.pickImage(source: ImageSource.camera);
-    setState(() {
-      _image = image;
-    });
-  }
+  // Future getImageFromCamera() async {
+  //   var image = await ImagePicker.platform.pickImage(source: ImageSource.camera);
+  //   setState(() {
+  //     _image = image;
+  //   });
+  // }
+
+  // Future getImageFromGallery() async {
+  //   var image = await ImagePicker.platform.pickImage(source: ImageSource.gallery);
+  //   setState(() {
+  //     _image = image;
+  //   });
+  // }
+
+  // Future getImagesFromGallery() async {
+  //   _images.clear();
+  //   final List<PickedFile> selectedImages = await ImagePicker.platform.pickMultiImage();
+  //   setState(() {
+  //     _images.addAll(selectedImages);
+  //   });
+  // }
+  
   Future getImageFromGallery() async {
-    var image = await ImagePicker.platform.pickImage(source: ImageSource.gallery);
+    List<Media> tempImages = await ImagesPicker.pick(
+      count: 5,
+      pickType: PickType.image
+    );
     setState(() {
-      _image = image;
+    _images.addAll(tempImages);
     });
   }
 }

438cd0737f1ce727b6ec502c44aabbe582cd3103
strong1133 <strong1133@naver.com>
2023-06-27 09:22:30 +0900

    코드팩토리 페이지네이션 oop 진행중

--- inflearn/actual/lib/common/model/pagination_params.dart
+++ inflearn/actual/lib/common/model/pagination_params.dart
@@ -9,6 +9,13 @@ class PaginationParams {
 
   const PaginationParams({this.after, this.count});
 
+  PaginationParams copyWith({
+    String? after,
+    int? count
+  }){
+    return PaginationParams(after: after??this.after, count: count?? this.count);
+  }
+
   factory PaginationParams.fromJson(Map<String, dynamic> json) => _$PaginationParamsFromJson(json);
   Map<String, dynamic> toJson() => _$PaginationParamsToJson(this);
 }
--- inflearn/actual/lib/restaurant/provider/restaurant_provider.dart
+++ inflearn/actual/lib/restaurant/provider/restaurant_provider.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/model/cursor_pagination_model.dart';
+import 'package:actual/common/model/pagination_params.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -16,8 +17,37 @@ class RestaurantStateNotifier extends StateNotifier<CursorPaginationBase> {
     pagenate();
   }
 
-  pagenate() async {
-    final resp = await repository.paginate();
+  pagenate({int fetchCount = 20, bool fetchMore = false, bool forceReFetch = false}) async {
+    // final resp = await repository.paginate();
+    // state = resp;
+
+    if (state is CursorPaginationModel && !forceReFetch) {
+      final pState = state as CursorPaginationModel;
+
+      if (!pState.meta.hasMore) {
+        return;
+      }
+    }
+
+    final isLoading = state is CursorPaginationLoading;
+    final isReFetching = state is CursorPaginationRefetching;
+    final isFetchingMore = state is CursorPaginationFetchingMore;
+
+    if (fetchMore && (isLoading || isReFetching || isFetchingMore)) {
+      return;
+    }
+
+    PaginationParams paginationParams = PaginationParams(count: fetchCount);
+
+    if (fetchMore) {
+      final pState = state as CursorPaginationModel;
+
+      state = CursorPaginationFetchingMore(meta: pState.meta, data: pState.data);
+
+      paginationParams = paginationParams.copyWith(after: pState.data.last.id);
+    }
+
+    final resp = await repository.paginate(paginationParams: paginationParams);
     state = resp;
   }
 }

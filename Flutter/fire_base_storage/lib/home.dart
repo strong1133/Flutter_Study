1a3d4d8cc6c20af064e0eb3ddd6cc542d36c54c8
strong1133 <strong1133@naver.com>
2023-07-29 21:07:14 +0900

    firebase 공부

--- Flutter/fire_base_storage/lib/home.dart
+++ Flutter/fire_base_storage/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:fire_base_storage/device_info.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
--- Flutter/fire_base_storage/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/fire_base_storage/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import firebase_core
+import firebase_database
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
 }
